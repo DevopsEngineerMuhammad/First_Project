d3ae9e0d2f4bd0a38653c8b2c92b0cb5adf87754
Muhammad Ullah <159222152+DevopsEngineerMuhammad@users.noreply.github.com>
2026-08-04 18:37:34 +0500

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -13,15 +13,9 @@ public class Greeter {
   }
   // Adding a new comment
   //TODO: Add javadoc comment
-   /** public String greet(String someone) {
+    public String greet(String someone) {
     return String.format("Hello, %s!", someone);
-  } */
-
-  public String greet(String someone) {
-    String password = "admin123";
-    return String.format("Hello, %s!", someone);
-}
-
+  } 
   
 }
 
